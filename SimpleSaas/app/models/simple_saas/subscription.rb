cc7dde2550462c676260f1b6a988283691c58bce
Mike Hart <mikedhart@gmail.com>
2013-03-01 02:05:48 +0000

    Jk

--- SimpleSaas/app/models/simple_saas/subscription.rb
+++ SimpleSaas/app/models/simple_saas/subscription.rb
@@ -1,6 +1,6 @@
 module SimpleSaas
   class Subscription < ActiveRecord::Base
-    attr_accessible :renew_at, :subscription_type_id, :user_id, :active
+    attr_accessible :renew_on, :subscription_type_id, :user_id, :active
 
     belongs_to :subscription_type
     #belongs_to :currency
