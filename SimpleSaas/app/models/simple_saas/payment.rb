a0a79270a0d5d42a58acdc61f8e442e1765cd583
Mike Hart <mikedhart@gmail.com>
2013-03-01 01:10:27 +0000

    kj

--- SimpleSaas/app/models/simple_saas/payment.rb
+++ SimpleSaas/app/models/simple_saas/payment.rb
@@ -1,6 +1,6 @@
 module SimpleSaas
   class Payment < ActiveRecord::Base
-    attr_accessible :amount, :comment, :currency_id, :payment_type_id, :rebased, :transaction_id, :subscription_id
+    attr_accessible :amount, :comment, :currency_id, :payment_type_id, :rebased, :transaction_id, :subscription_id, :payment_status
 
     belongs_to :subscription
     belongs_to :currency
